1224e2d98ba306680da696f068866c7a4aac26f7
DaRaLe <danieye2188@gmail.com>
2026-02-06 12:09:30 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,16 @@
+import ApplicationForm from "@/components/ApplicationForm";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Bewerbung schreiben – einfach & professionell</h1>
+      <p>
+        Beantworte ein paar kurze Fragen. Wir erstellen daraus eine moderne,
+        seriöse Bewerbung, die du sofort verwenden kannst.
+      </p>
+
+      <ApplicationForm />
+    </>
+  );
+}
 
