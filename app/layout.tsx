0651a761a97df2fbf56a90ca5157d7d6dde71177
DaRaLe <danieye2188@gmail.com>
2026-02-06 12:06:04 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
+import "../styles/globals.css";
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="de">
+      <body>
+        <div className="container">
+          <header className="header">
+            <strong>EinfachBewerben</strong>
+          </header>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
